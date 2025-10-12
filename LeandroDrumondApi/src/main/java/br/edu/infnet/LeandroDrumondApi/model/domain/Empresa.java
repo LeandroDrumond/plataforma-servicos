7967eac5d13463225b2f12e4dac34dd78c2c8c1c
Leandro Drumond Silveira Vianna <esx.lvianna@terceiros.mag.com.br>
2025-10-12 20:48:51 -0300

    criando classe Empresa

---
+++ LeandroDrumondApi/src/main/java/br/edu/infnet/LeandroDrumondApi/model/domain/Empresa.java
@@ -0,0 +1,34 @@
+package br.edu.infnet.LeandroDrumondApi.model.domain;
+
+public class Empresa {
+
+	    private String id;
+	    private String nome;
+	    private String email;
+	    private String endereco;
+	    
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getEndereco() {
+			return endereco;
+		}
+		public void setEndereco(String endereco) {
+			this.endereco = endereco;
+		}
+}
